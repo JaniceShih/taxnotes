9cc603455c9f8d1e051f91824f30a725d9d1c2e9
JaniceShih <janice.hc.shih@gmail.com>
2022-10-14 06:14:27 -0400

    date format

--- src/components/Nav/NavBar.jsx
+++ src/components/Nav/NavBar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const NavBar = () => {
 
   const current = new Date();
-  const date = `${current.getDate()}/${current.getMonth()+1}/${current.getFullYear()}`;
+  const date = `${current.getMonth()+1}/${current.getDate()}/${current.getFullYear()}`;
   return (
   <>
     <div className='top-nav_topNav'> 
